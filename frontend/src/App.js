2188e9575914823534ad6cc95a161dee889c6891
Darius Seals <tristarmediagroupinc@gmail.com>
2022-08-21 15:57:00 -0400

    init commit

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+﻿import React from "react";
+import { Switch, Route } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
+
+import Home from "./components/Home/Home";
+
+//TODO Web Template Studio: Add routes for your new pages here.
+const App = () => {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <Switch>
+          <Route exact path = "/" component = { Home } />
+        </Switch>
+        <Footer />
+      </React.Fragment>
+    );
+}
+
+export default App;
